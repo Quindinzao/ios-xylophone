885dfc6b3b25e57f6ceb39495dc8a6ab68001a38
Quindinzao <devjaodev@gmail.com>
2024-08-29 23:50:57 -0300

    first commit

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,36 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+    @IBAction func changeBackgroundColor(_ sender: UIButton) {
+        sender.alpha = 0.5
+    }
 
-    
+    @IBAction func keyPressed(_ sender: UIButton) {
+        sender.alpha = 1
+        playSound(musicalNote: sender.currentTitle!)
+        
+        print("Start")
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2, execute: {
+            print("End")
+        })
+    }
 
+    func playSound(musicalNote: String) {
+        let path = Bundle.main.path(forResource: musicalNote, ofType:"wav")
+        let url = URL(fileURLWithPath: path!)
+        
+        player = try! AVAudioPlayer(contentsOf: url)
+        player?.play()
+    }
 }
 
